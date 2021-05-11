865bd45be8a5801567cf6e650d7ff54df4f4d8f9
hvinoy <vinoyharsha@gmail.com>
2021-05-11 00:00:58 -0400

    updates

--- employeeSQL/employee2.sql
+++ employeeSQL/employee2.sql
@@ -4,7 +4,7 @@
 
 --drop table departments
 --drop table dept_emp
-drop table dept_manager
+--drop table dept_manager
 --drop table employees
 --drop table salaries
 --drop table titles
@@ -16,19 +16,8 @@ CREATE TABLE "departments" (
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
+	
 );
-
-CREATE TABLE "dept_emp" (
-    "emp_no" integer   NOT NULL,
-    "dept_no" varchar(20)   NOT NULL
-    
-);
-
-CREATE TABLE "dept_manager" (
-    "dept_no" varchar(20)   NOT NULL,
-    "emp_no" integer   NOT NULL   
-);
-
 CREATE TABLE "employees" (
     "emp_no" integer   NOT NULL,
     "emp_title_id" varchar(20)   NOT NULL,
@@ -41,6 +30,25 @@ CREATE TABLE "employees" (
         "emp_no"
      )
 );
+
+
+CREATE TABLE "dept_emp" (
+    "emp_no" integer   NOT NULL,
+    "dept_no" varchar(20)   NOT NULL,
+    PRIMARY KEY ("emp_no", "dept_no"),
+		
+);
+
+
+
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar(20)   NOT NULL,
+    "emp_no" integer   NOT NULL,
+	primary key (dept_no,emp_no)
+);
+
+
 
 CREATE TABLE "salaries" (
     "emp_no" integer   NOT NULL,
@@ -57,4 +65,70 @@ CREATE TABLE "titles" (
         "title_id"
      )
 );
+
+alter table employees
+add foreign key (emp_title_id) references titles(title_id);
+ 
+alter table dept_emp
+add FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+add foreign key (dept_no) references departments(dept_no)
+;
+
+alter table dept_manager
+add foreign key (dept_no) references departments(dept_no),
+add foreign key (emp_no) references employees(emp_no);
+
+alter table salaries
+add foreign key (emp_no) references employees(emp_no);
+
+
+select * from titles
+
+----List the following details of each employee: employee number, last name, first name, sex, and salary.
+create view q_1 as
+select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+from employees
+left join salaries
+on employees.emp_no = salaries.emp_no;
+
+---List first name, last name, and hire date for employees who were hired in 1986.
+create view q_2 as
+select first_name, last_name, hire_date
+from employees 
+where hire_date LIKE '%86% ;
+
+----List the manager of each department with the following information: 
+---department number, department name, the manager's employee number, last name, first name.
+create view q_3 as
+select m.dept_no, departments.dept_name,m.emp_no,employees.last_name, employees.first_name
+from dept_manager m
+left join departments 
+on m.dept_no = departments.dept_no
+left join employees
+on m.emp_no = employees.emp_no;
+
+---List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+
+create view q_4 as
+select e.emp_no, e.last_name,e.first_name
+from employees e;
+
+
+---List first name, last name, and sex for employees whose first name is "Hercules" 
+---and last names begin with "B."
+create view q_5 as
+select e.first_name, e.last_name, e.sex
+from employees e
+where first_name = 'Hercules'
+and last_name like 'B%';
+
+---List all employees in the Sales department, 
+--including their employee number, last name, first name, and department name.
+
+--subqueries??
+select employees.emp_no, employees.last_name, employees.first_name,
+select dept_no
+from departments
+where dept_name = 'Sales';
 
